1034b72bf40f47cb2a96f12e833449ca1023d97d
maxregnerklos <135330858+maxregnerklos@users.noreply.github.com>
2024-07-09 08:43:04 +0200

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -f recovery.img.lz4 ];then
-	lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
-fi
-
 off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
 dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
 
